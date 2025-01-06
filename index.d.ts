973972b29486e84a7c8ee739c7a6d2fbce935fe2
yasser <dev@yasser.email>
2025-01-06 17:27:02 -0300

    Add TypeScript typings (#2)
    Co-authored-by: Kasper Isager Dalsgarð <kasperisager@hey.com>

---
+++ index.d.ts
@@ -0,0 +1,89 @@
+import EventEmitter from 'bare-events'
+import Buffer from 'bare-buffer'
+
+export interface RemoteInfo {
+  address: string
+  family: 'IPv4' | 'IPv6'
+  port: number
+  size: number
+}
+
+export class Socket extends EventEmitter<{
+  close: []
+  connect: []
+  error: [err: Error]
+  listening: []
+  message: [message: string | Buffer, remote: RemoteInfo]
+}> {
+  constructor(opts?: { ipv6Only?: boolean; reuseAddress?: boolean })
+
+  address(): { address: string; family: string; port: number } | null
+
+  remoteAddress(): { address: string; family: string; port: number } | null
+
+  bind(port: number, address: string, cb?: () => void): this
+  bind(opts: { port?: number; address?: string }, cb?: () => void): this
+  bind(port: number, cb?: () => void): this
+  bind(cb?: () => void): this
+
+  connect(port: number, address: string, cb?: () => void): void
+  connect(port: number, cb?: () => void): void
+
+  close(cb?: (err: Error) => void): Promise<void>
+
+  send(
+    msg: string | Buffer,
+    offset: number,
+    length: number,
+    port?: number,
+    address?: string,
+    cb?: (err: Error) => void
+  ): Promise<void>
+
+  send(
+    msg: string | Buffer,
+    offset: number,
+    length: number,
+    port: number,
+    cb: (err: Error) => void
+  ): Promise<void>
+
+  send(
+    msg: string | Buffer,
+    offset: number,
+    length: number,
+    address: string,
+    cb: (err: Error) => void
+  ): Promise<void>
+
+  send(
+    msg: string | Buffer,
+    offset: number,
+    address: string,
+    cb: (err: Error) => void
+  ): Promise<void>
+
+  send(
+    msg: string | Buffer,
+    offset: number,
+    length: number,
+    cb: (err: Error) => void
+  ): Promise<void>
+
+  send(msg: string | Buffer, port: number, address: string): Promise<void>
+
+  send(
+    msg: string | Buffer,
+    port: number,
+    cb?: (err: Error) => void
+  ): Promise<void>
+
+  send(msg: string | Buffer, address: string): Promise<void>
+
+  send(msg: string | Buffer, cb: (err: Error) => void): Promise<void>
+}
+
+export function createSocket(
+  opts?: { ipv6Only?: boolean; reuseAddress?: boolean } | string,
+  cb?: (message: Buffer, remote: RemoteInfo) => void
+): Socket
